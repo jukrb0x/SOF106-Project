b4b9c0e6b2206ad516d53c86d9ce03f1d36ab8b3
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-07-03 00:32:48 +0800

    refactor(backend): minor changes

--- backend/digit_recognition/views.py
+++ backend/digit_recognition/views.py
@@ -13,9 +13,6 @@ def index(request):
     :return:
     """
     context = {'time': time.asctime(time.localtime(time.time()))}
-    _type = "Type of request: " + str(type(request))
-    print(_type)
-    print("Context Dict: " + str(context))
     # eject the context to the template
     return render(request, 'index.html', context)
 
@@ -29,15 +26,10 @@ def api_root_handler(request):
     api_html_template = """
     <html><body><pre>Welcome to API root</pre></body></html>
     """
+    _type = "Type of request: " + str(type(request))
+    print(_type)
     ret = api_html_template
     if request.method == 'POST':
         return HttpResponse(ret)
     else:
         return HttpResponse(ret)
-
-
-def api_root_test_handler(request):
-    context = {'time': time.asctime(time.localtime(time.time()))}
-    return render(request, 'api-tester.html', context)
-
-

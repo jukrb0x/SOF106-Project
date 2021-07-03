2b81a4e19e4c2a73da804c466e3a0e5b4b1674f1
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-07-03 18:39:13 +0800

    feat(backend): shutdown django database

--- backend/digit_recognition/settings.py
+++ backend/digit_recognition/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'digit_recognition.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation

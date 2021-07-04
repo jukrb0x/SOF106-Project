dcd397769be5d2244b0508facd5b834cd5bd93d9
jukrb0x <15688641+jukrb0x@users.noreply.github.com>
2021-07-05 01:37:59 +0800

    refactor(frontend): separate welcome dialog to a component
    - use vuex to ensure the communication between parent and child components

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -57,43 +57,7 @@
     <q-page-container>
       <router-view/>
       <!--  Welcome Dialog  -->
-      <q-dialog v-model="welcomeDialog">
-        <q-card>
-          <q-toolbar>
-            <q-avatar>
-              <img src="~assets/XMUM.png">
-            </q-avatar>
-            <q-toolbar-title><span class="text-weight-bold">SOF106</span> Project</q-toolbar-title>
-            <q-btn flat round dense icon="close" v-close-popup/>
-          </q-toolbar>
-          <q-card-section>
-            <div style="max-width: 550px">
-              <p class="text-subtitle2">Presented by Group 2-11</p>
-              Welcome to the SOF106 PAI Project - <b>Handwritten digit recognition</b>
-              <div class="q-pt-sm">
-                This is the frontend server deployed on Vercel, due to the <a
-                href="http://devcenter.heroku.com/articles/slug-size" target="_blank">slug-size</a> of backend,<br>
-                we did not deploy it on a cloud server. You can download the backend source code (Python) and start the
-                server on your machine, it will open the port
-                <pre style="display: inline">8000</pre>
-                by default. Or you can deploy it on a cloud server and put the host address in the settings, which you
-                may
-                see later.
-              </div>
-              <div class="q-mt-sm">
-                This program currently only supports the single integer digit recognition, the output range is
-                <pre style="display: inline">[0,9]</pre>
-                .
-              </div>
-              <div class="q-mt-lg">
-                Check our <a :href="githubRepo" target="_blank">
-                <q-icon name="fab fa-github"></q-icon>
-                Github Repository</a> for more details
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
-      </q-dialog>
+      <WelcomeDialog :welcome-dialog-state="welcomeDialog"/>
       <!--  footer  -->
       <q-footer bordered class="bg-white text-black row justify-center q-pa-lg-sm">
         <div class="github-link q-ma-sm">
@@ -111,10 +75,11 @@
 import { Screen } from 'quasar'
 import { computed, defineComponent, ref } from '@vue/composition-api';
 import EssentialLink from "components/EssentialLink.vue";
+import WelcomeDialog from "components/WelcomeDialog.vue";
 
 export default defineComponent({
   name: 'MainLayout',
-  components: { EssentialLink },
+  components: { WelcomeDialog, EssentialLink },
   mounted() {
     this.leftDrawerOpen = this.drawerOpen()
   },
@@ -124,8 +89,6 @@ export default defineComponent({
     }
   },
   setup() {
-    const githubRepo = ref('')
-    githubRepo.value = "https://github.com/jukrb0x/SOF106-Project";
     const welcomeDialog = ref(true);
     const leftDrawerOpen = ref(false);
     // computed var is read-only
@@ -136,7 +99,7 @@ export default defineComponent({
     const team_name = ref('');
     name.value = "Digit Recognition";
     team_name.value = "PAI Group 2-11";
-    return { welcomeDialog, leftDrawerOpen, name, team_name, githubRepo }
+    return { welcomeDialog, leftDrawerOpen, name, team_name }
   }
 });
 </script>
